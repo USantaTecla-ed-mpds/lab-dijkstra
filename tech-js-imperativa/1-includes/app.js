e3adee2836b7287844f9374eaf555687fd630a90
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-27 19:25:52 +0200

    Delete tech-js-imperativa/1-includes directory

--- tech-js-imperativa/1-includes/app.js
+++
@@ -1,7 +0,0 @@
-const { Console } = require("console-mpds");
-const console = new Console();
-
-const min = console.readNumber(`Introduce el mínimo del intervalo: `);
-const max = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
-const point = console.readNumber(`Introduce un punto:`);
-console.writeln(`El intervalo [${min}, ${max}] ${point >= min && point <= max ? ` sí` : ` no`} incluye el punto ${point}`);
