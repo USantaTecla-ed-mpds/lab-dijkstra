0988893945efec903e79dd8a976b2752eef47100
Andres <andrespodadera87@gmail.com>
2022-04-27 19:41:23 +0200

    Add files via upload

---
+++ tech-js-imperativa/0-fraction/3-substract.js
@@ -0,0 +1,16 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+console.writeln(`Primera fracción:`);
+let num1 = console.readNumber(`Introduce el numerador de la fracción:`);
+let den1 = console.readNumber(`Introduce el denominador de la fracción:`);
+
+console.writeln(`Segunda fracción:`);
+let num2 = console.readNumber(`Introduce el numerador de la fracción:`);
+let den2 = console.readNumber(`Introduce el denominador de la fracción:`);
+
+console.writeln(
+  `La resta de la fracción ${num1}/${den1} y la fracción ${num2}/${den2} es la fracción ${
+    num1 * den2 - den1 * num2
+  }/${den1 * den2}`
+);
