3616786128166aa40e0b7c1053ab5ec304105898
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-27 19:26:02 +0200

    Delete tech-js-imperativa/2-scale directory

--- tech-js-imperativa/2-scale/app.js
+++
@@ -1,9 +0,0 @@
-const { Console } = require("console-mpds");
-const console = new Console();
-
-const min = console.readNumber(`Introduce el mínimo del intervalo: `);
-const max = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
-const scaleFactor = console.readNumber(`Introduce un factor de escala positivo:`);
-const newMin = min * scaleFactor;
-const newMax = max * scaleFactor;
-console.writeln(`El nuevo intervalo es [${newMin}, ${newMax}]`);
