3a52ec5f46472b29725d241f7fddc1d0b14424ad
Andres <andrespodadera87@gmail.com>
2022-04-27 23:49:49 +0200

    Update app.js

--- tech-js-imperativa/1-interval/2-scale/app.js
+++ tech-js-imperativa/1-interval/2-scale/app.js
@@ -1,9 +1,19 @@
+//Para admitir escalas decimales, en el archivo console.js el metodo readnumber usa parseInt y debería usar parseFloat
+
 const { Console } = require("console-mpds");
 const console = new Console();
 
 const min = console.readNumber(`Introduce el mínimo del intervalo: `);
-const max = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
-const scaleFactor = console.readNumber(`Introduce un factor de escala positivo:`);
-const newMin = min * scaleFactor;
-const newMax = max * scaleFactor;
-console.writeln(`El nuevo intervalo es [${newMin}, ${newMax}]`);
+const max = console.readNumber(
+  `Introduce el máximo del intervalo (superior o igual al mínimo): `
+);
+const scaleFactor = console.readNumber(
+  `Introduce un factor de escala positivo:`
+);
+const interval = max - min;
+const centerInterval = interval / 2 + min;
+const newMin = centerInterval - (interval / 2) * scaleFactor;
+const newMax = centerInterval + (interval / 2) * scaleFactor;
+console.writeln(
+  `El intervalo [${min}, ${max}] con factor de escala ${scaleFactor} es el intervalo [${newMin}, ${newMax}]`
+);
