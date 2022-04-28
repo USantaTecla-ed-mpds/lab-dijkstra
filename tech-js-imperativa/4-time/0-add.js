bbfd851a989bea925bef3e7497df699ce535bd91
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 11:40:30 +0200

    Update 0-add.js

--- tech-js-imperativa/4-time/0-add.js
+++ tech-js-imperativa/4-time/0-add.js
@@ -18,10 +18,10 @@ ${msgDay}`);
 const minutesSecond = console.readNumber(`${msgMonth}`);
 const secondsSecond = console.readNumber(`${msgYear}`);
 
-const sum = (hoursFirst + hoursSecond) * SECONDS_HOUR + (minutesFirst + minutesSecond) * SECONDS_MINUTE + secondsFirst + secondsSecond;
+const totalSeconds = (hoursFirst + hoursSecond) * SECONDS_HOUR + (minutesFirst + minutesSecond) * SECONDS_MINUTE + secondsFirst + secondsSecond;
 
-const totalHours = sum / SECONDS_HOUR - (sum % SECONDS_HOUR / SECONDS_HOUR);
-const totalMinutes = sum % SECONDS_HOUR / SECONDS_MINUTE - (sum % SECONDS_HOUR % SECONDS_MINUTE / SECONDS_MINUTE);
-const totalSeconds = sum % SECONDS_HOUR % SECONDS_MINUTE;
+const sumHours = totalSeconds / SECONDS_HOUR - (totalSeconds % SECONDS_HOUR / SECONDS_HOUR);
+const sumMinutes = totalSeconds % SECONDS_HOUR / SECONDS_MINUTE - (totalSeconds % SECONDS_HOUR % SECONDS_MINUTE / SECONDS_MINUTE);
+const sumSeconds = totalSeconds % SECONDS_HOUR % SECONDS_MINUTE;
 
-console.writeln(`La suma es ${totalHours}:${totalMinutes}:${totalSeconds}`);
+console.writeln(`La suma es ${sumHours}:${sumMinutes}:${sumSeconds}`);
