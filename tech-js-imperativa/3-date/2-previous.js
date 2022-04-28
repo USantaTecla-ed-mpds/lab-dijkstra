bb8057ce43b6c5464f5771d02072efc127b6f7a8
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 08:42:31 +0200

    Update 2-previous.js

--- tech-js-imperativa/3-date/2-previous.js
+++ tech-js-imperativa/3-date/2-previous.js
@@ -1,14 +1,14 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-const dia = console.readNumber(`Dame el día:`);
-const mes = console.readNumber(`Dame el mes:`);
-const anyo = console.readNumber(`Dame el año:`);
+const day = console.readNumber(`Dame el día: `);
+const month = console.readNumber(`Dame el mes: `);
+const year = console.readNumber(`Dame el año: `);
 
-console.write(
-  `La fecha es ${dia}/${mes}/${anyo} y la anterior es ${
-    dia === 1 ? 31 : dia - 1
-  }/${dia === 1 && mes - 1 !== 0 ? mes - 1 : 12}/${
-    dia === 1 && mes === 1 ? anyo - 1 : anyo
-  } `
-);
+let monthDatePrevious = day === 1 ? month - 1 : month;
+monthDatePrevious = monthDatePrevious === 0 ? 12 : monthDatePrevious;
+daysMonth = monthDatePrevious === 2 ? (year % 4 === 0 ? 29 : 28) : (monthDatePrevious === 4 || monthDatePrevious === 6 || monthDatePrevious === 9 || monthDatePrevious === 11 ? 30 : 31);
+let dayDatePrevious =  day === 1  ? daysMonth : day - 1;
+let yearDatePrevious = day === 1  && month === 1 ? year - 1 : year;
+
+console.writeln(`La fecha ${day}/${month}/${year} y el anterior es ${dayDatePrevious}/${monthDatePrevious}/${yearDatePrevious}`);
