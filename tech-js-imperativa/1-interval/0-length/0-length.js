33d4d45dad90f29502353f8de3e905c84b805397
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 08:55:53 +0200

    Delete tech-js-imperativa/1-interval/0-length directory

--- tech-js-imperativa/1-interval/0-length/0-length.js
+++
@@ -1,6 +0,0 @@
-const { Console } = require("console-mpds");
-const console = new Console();
-
-const min = console.readNumber(`Introduce el mínimo del intervalo: `);
-const max = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
-console.writeln(`La longitud del intervalo [${min}, ${max}] es ${max - min}`);
