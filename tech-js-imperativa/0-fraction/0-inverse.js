acef04b6e377202ba9efbbe6b5702958165ae5cf
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 10:02:40 +0200

    Update 0-inverse.js

--- tech-js-imperativa/0-fraction/0-inverse.js
+++ tech-js-imperativa/0-fraction/0-inverse.js
@@ -3,6 +3,4 @@ const console = new Console();
 
 let num = console.readNumber(`Introduce el numerador de la fracción:`);
 let den = console.readNumber(`Introduce el denominador de la fracción:`);
-console.writeln(
-  `La fracción ${num}/${den} invertida es la fracción ${den}/${num}`
-);
+console.writeln(`La fracción ${num}/${den} invertida es la fracción ${den}/${num}`);
