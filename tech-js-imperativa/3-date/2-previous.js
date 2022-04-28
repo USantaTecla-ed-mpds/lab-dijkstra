7064fd3fe550dbbb5a97217d1fbe49a20759c675
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 08:52:52 +0200

    Update 2-previous.js

--- tech-js-imperativa/3-date/2-previous.js
+++ tech-js-imperativa/3-date/2-previous.js
@@ -5,10 +5,9 @@ const day = console.readNumber(`Dame el día: `);
 const month = console.readNumber(`Dame el mes: `);
 const year = console.readNumber(`Dame el año: `);
 
-let monthDatePrevious = day === 1 ? month - 1 : month;
-monthDatePrevious = monthDatePrevious === 0 ? 12 : monthDatePrevious;
-daysMonth = monthDatePrevious === 2 ? (year % 4 === 0 ? 29 : 28) : (monthDatePrevious === 4 || monthDatePrevious === 6 || monthDatePrevious === 9 || monthDatePrevious === 11 ? 30 : 31);
-let dayDatePrevious =  day === 1  ? daysMonth : day - 1;
-let yearDatePrevious = day === 1  && month === 1 ? year - 1 : year;
+let datePreviousMonth = day === 1 ? month - 1 : month;
+datePreviousMonth = datePreviousMonth === 0 ? 12 : datePreviousMonth;
+let datePreviousDay = day === 1  ? 30 : day - 1;
+let datePreviousYear = day === 1  && month === 1 ? year - 1 : year;
 
-console.writeln(`La fecha ${day}/${month}/${year} y el anterior es ${dayDatePrevious}/${monthDatePrevious}/${yearDatePrevious}`);
+console.writeln(`La fecha ${day}/${month}/${year} y el anterior es ${datePreviousDay}/${datePreviousMonth}/${datePreviousYear}`);
