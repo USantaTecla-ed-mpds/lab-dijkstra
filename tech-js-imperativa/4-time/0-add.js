ca3c5737732ea1b406423db61e68ac9a377cc824
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 09:24:37 +0200

    Update 0-add.js

--- tech-js-imperativa/4-time/0-add.js
+++ tech-js-imperativa/4-time/0-add.js
@@ -1,20 +1,27 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-console.writeln(`Primera duración:`);
-const hours = console.readNumber(`Dame las horas:`);
-const minutes = console.readNumber(`Dame las minutos:`);
-const seconds = console.readNumber(`Dame las segundos:`);
+const SECONDS_HOUR = 3600;
+const SECONDS_MINUTE = 60;
 
-console.writeln(`\nSegunda duración:`);
-const hours2 = console.readNumber(`Dame las horas:`);
-const minutes2 = console.readNumber(`Dame las minutos:`);
-const seconds2 = console.readNumber(`Dame las segundos:`);
+const msgDay = `Dame las horas:`;
+const msgMonth = `Dame los minutos:`;
+const msgYear = `Dame los segundos:`;
 
-console.writeln(
-  `La suma es ${
-    minutes + minutes2 > 60 ? hours + hours2 + 1 : hours + hours2
-  }:${seconds + seconds2 > 60 ? minutes + minutes2 - 60 : minutes + minutes2}:${
-    seconds + seconds2 > 60 ? minutes + minutes2 - 60 : minutes + minutes2
-  }`
-);
+const hoursFirst = console.readNumber(`Primera duración:
+${msgDay}`);
+const minutesFirst = console.readNumber(`${msgMonth}`);	
+const secondsFirst = console.readNumber(`${msgYear}`);
+
+const hoursSecond = console.readNumber(`Segunda duración:
+${msgDay}`);
+const minutesSecond = console.readNumber(`${msgMonth}`);
+const secondsSecond = console.readNumber(`${msgYear}`);
+
+const sum = (hoursFirst + hoursSecond) * SECONDS_HOUR + (minutesFirst + minutesSecond) * SECONDS_MINUTE + secondsFirst + secondsSecond;
+
+const totalHours = sum / SECONDS_HOUR - (sum % SECONDS_HOUR / SECONDS_HOUR);
+const totalMinutes = sum % SECONDS_HOUR / SECONDS_MINUTE - (sum % SECONDS_HOUR % SECONDS_MINUTE / SECONDS_MINUTE);
+const totalSeconds = sum % SECONDS_HOUR % SECONDS_MINUTE;
+
+console.writeln(`La suma es ${totalHours}:${totalMinutes}:${totalSeconds}`);
