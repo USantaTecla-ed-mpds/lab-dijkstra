4572e2bca7b299c846fb64803d10a221671e3395
Andrés Podadera <andrespodadera87@gmail.com>
2022-04-28 15:47:52 +0200

    Update 0-valid.js

--- tech-js-imperativa/3-date/0-valid.js
+++ tech-js-imperativa/3-date/0-valid.js
@@ -1,12 +1,10 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-let dia = console.readNumber(`Dame el día:`);
-let mes = console.readNumber(`Dame el mes:`);
-let anyo = console.readNumber(`Dame el año:`);
+let day = console.readNumber(`Dame el día:`);
+let mounth = console.readNumber(`Dame el mes:`);
+let year = console.readNumber(`Dame el año:`);
 
 console.write(
-  `La fecha ${dia}/${mes}/${anyo} ${
-    dia <= 31 && mes <= 12 ? "sí" : "no"
-  } es válida`
+  `La fecha ${day}/${mounth}/${year} ${day <= 31 && mounth <= 12 ? "sí" : "no"} es válida`
 );
