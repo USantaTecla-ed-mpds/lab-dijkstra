5cddafb40b6a19aef5b9d4d224e395670dafca7c
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 08:57:06 +0200

    Create 2-scale.js

---
+++ tech-js-imperativa/1-interval/2-scale.js
@@ -0,0 +1,17 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+const min = console.readNumber(`Introduce el mínimo del intervalo: `);
+const max = console.readNumber(
+  `Introduce el máximo del intervalo (superior o igual al mínimo): `
+);
+const scaleFactor = console.readNumber(
+  `Introduce un factor de escala positivo:`
+);
+const interval = max - min;
+const centerInterval = interval / 2 + min;
+const newMin = centerInterval - (interval / 2) * scaleFactor;
+const newMax = centerInterval + (interval / 2) * scaleFactor;
+console.writeln(
+  `El intervalo [${min}, ${max}] con factor de escala ${scaleFactor} es el intervalo [${newMin}, ${newMax}]`
+);
