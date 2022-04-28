56f3e000dfbe74727b2cc9ce6e585ea5edff1095
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 10:02:23 +0200

    Update 2-scale.js

--- tech-js-imperativa/1-interval/2-scale.js
+++ tech-js-imperativa/1-interval/2-scale.js
@@ -2,12 +2,8 @@ const { Console } = require("console-mpds");
 const console = new Console();
 
 const min = console.readNumber(`Introduce el mínimo del intervalo: `);
-const max = console.readNumber(
-  `Introduce el máximo del intervalo (superior o igual al mínimo): `
-);
-const scaleFactor = console.readString(
-  `Introduce un factor de escala positivo:`
-);
+const max = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
+const scaleFactor = console.readString(`Introduce un factor de escala positivo:`);
 
 const centerOfInterval = (min + max) / 2;
 const distanceToCenter = centerOfInterval - min;
