b7eb2eadc831463b2af790f581235f701f1d2555
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 10:03:53 +0200

    Update 4-multiply.js

--- tech-js-imperativa/0-fraction/4-multiply.js
+++ tech-js-imperativa/0-fraction/4-multiply.js
@@ -10,8 +10,4 @@ console.writeln(`Segunda fracción:`);
 let num2 = console.readNumber(`Introduce el numerador de la fracción:`);
 let den2 = console.readNumber(`Introduce el denominador de la fracción:`);
 
-console.writeln(
-  `La multiplicación de la fracción ${num1}/${den1} y la fracción ${num2}/${den2} es la fracción ${
-    num1 * num2
-  }/${den1 * den2}`
-);
+console.writeln(`La multiplicación de la fracción ${num1}/${den1} y la fracción ${num2}/${den2} es la fracción ${num1 * num2}/${den1 * den2}`);
