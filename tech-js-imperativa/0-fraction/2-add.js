aa8055f74ff8fcfdd13fd3d2e0e7cac8c828be99
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-04-28 10:03:09 +0200

    Update 2-add.js

--- tech-js-imperativa/0-fraction/2-add.js
+++ tech-js-imperativa/0-fraction/2-add.js
@@ -9,8 +9,4 @@ console.writeln(`Segunda fracción:`);
 let num2 = console.readNumber(`Introduce el numerador de la fracción:`);
 let den2 = console.readNumber(`Introduce el denominador de la fracción:`);
 
-console.writeln(
-  `La suma de la fracción ${num1}/${den1} y la fracción ${num2}/${den2} es la fracción ${
-    num1 * den2 + num1 * den2
-  }/${den1 * den2}`
-);
+console.writeln(`La suma de la fracción ${num1}/${den1} y la fracción ${num2}/${den2} es la fracción ${num1 * den2 + num1 * den2}/${den1 * den2}`);
