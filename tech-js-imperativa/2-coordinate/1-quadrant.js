1ea7fd7ab641bc7e260641754f7d7f4e91e3a1b3
Andrés Podadera <andrespodadera87@gmail.com>
2022-05-02 16:57:05 +0200

    Update 1-quadrant.js

--- tech-js-imperativa/2-coordinate/1-quadrant.js
+++ tech-js-imperativa/2-coordinate/1-quadrant.js
@@ -1,20 +1,14 @@
 const { Console } = require("console-mpds");
 const console = new Console();
-const abciseCoordinate = console.readNumber(
-  `Dame la abcisa de la coordenada: `
-);
+const abciseCoordinate = console.readNumber(`Dame la abcisa de la coordenada: `);
 
-const ordinateCoordinate = console.readNumber(
-  `Dame la ordenada de la coordenada: `
-);
-let quadrant =
-  abciseCoordinate > 0 && ordinateCoordinate > 0
-    ? 1
-    : ordinateCoordinate < 0 && abciseCoordinate < 0
-    ? 3
-    : abciseCoordinate < 0
-    ? 2
-    : 4;
+const ordinateCoordinate = console.readNumber(`Dame la ordenada de la coordenada: `);
+
+const quadrant = abciseCoordinate > 0 && ordinateCoordinate > 0
+    ? 1 : ordinateCoordinate < 0 && abciseCoordinate < 0
+    ? 3 : abciseCoordinate < 0
+    ? 2 : 4;
+
 console.writeln(
   `La coordenada (${abciseCoordinate}, ${ordinateCoordinate}) está en el ${quadrant}º cuadrante`
 );
