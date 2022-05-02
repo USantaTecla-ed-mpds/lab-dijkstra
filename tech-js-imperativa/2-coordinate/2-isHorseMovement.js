358f8ac11ddd9081e8d2f2b2bad77133c0a084da
Andrés Podadera <andrespodadera87@gmail.com>
2022-05-02 16:54:13 +0200

    Update 2-isHorseMovement.js

--- tech-js-imperativa/2-coordinate/2-isHorseMovement.js
+++ tech-js-imperativa/2-coordinate/2-isHorseMovement.js
@@ -3,9 +3,11 @@ const console = new Console();
 
 const LONG_MOVE = 2;
 const SHORT_MOVE = 1;
+
 console.writeln(`Coordenada origen:`);
 const abcissaOrigin = console.readNumber(`Dame la abcisa de la coordenada:`);
 const ordinateOrigin = console.readNumber(`Dame la ordenada de la coordenada:`);
+
 console.writeln(`Coordenada destino:`);
 const abcissaDestiny = console.readNumber(`Dame la abcisa de la coordenada:`);
 const ordinateDestiny = console.readNumber(`Dame la ordenada de la coordenada:`);
