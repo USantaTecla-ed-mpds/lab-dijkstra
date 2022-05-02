d28c86c9f05d789eda20fcfa605c6eb5a39dac40
Andrés Podadera <andrespodadera87@gmail.com>
2022-05-02 16:57:50 +0200

    Update 2-isHorizontal.js

--- tech-js-imperativa/2-coordinate/2-isHorizontal.js
+++ tech-js-imperativa/2-coordinate/2-isHorizontal.js
@@ -2,11 +2,11 @@ const { Console } = require("console-mpds");
 const console = new Console();
 
 console.writeln(`Coordenada origen:`);
-let absOrigin = console.readNumber(`Dame la abcisa de la coordenada:`);
-let ordOrigin = console.readNumber(`Dame la ordenada de la coordenada:`);
+const absOrigin = console.readNumber(`Dame la abcisa de la coordenada:`);
+const ordOrigin = console.readNumber(`Dame la ordenada de la coordenada:`);
 console.writeln(`Coordenada destino:`);
-let absDestiny = console.readNumber(`Dame la abcisa de la coordenada:`);
-let ordDestiny = console.readNumber(`Dame la ordenada de la coordenada:`);
+const absDestiny = console.readNumber(`Dame la abcisa de la coordenada:`);
+const ordDestiny = console.readNumber(`Dame la ordenada de la coordenada:`);
 
 console.writeln(
   `La coordenada oringen (${absOrigin}, ${ordOrigin}) y la coordenada destino (${absDestiny}, ${ordDestiny}) ${
