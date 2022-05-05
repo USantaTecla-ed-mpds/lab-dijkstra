bdc1163447e8f30a3bc1ba6bdf6293a9e46c2b3c
Andrés Podadera <andrespodadera87@gmail.com>
2022-05-05 18:42:03 +0200

    Update 0-1-inverse.js

--- tech-js-estructurada/0-fraction/0-1-inverse.js
+++ tech-js-estructurada/0-fraction/0-1-inverse.js
@@ -1,35 +1,3 @@
-// const { Console } = require("console-mpds");
-// const console = new Console();
-
-// const numerator = console.readNumber(`Introduce el numerador de la fracción: `);
-// const denominator = console.readNumber(`Introduce el denominador de la fracción: `);
-// let greatestCommonDivisor;
-
-// if (numerator > denominator){
-//     let i = denominator;
-//     while(i > 1){
-//          if (denominator%i ===0){
-//             if (numerator%i === 0)
-//                 greatestCommonDivisor = i;        
-//             }
-//         i--;
-//     }
-// }else if (numerator < denominator){
-//     let i = numerator;
-//     while(i > 1){
-//          if (numerator%i ===0){
-//             if (denominator%i === 0)
-//                 greatestCommonDivisor = i;        
-//             }
-//         i--;
-//     }
-// }else {
-//     greatestCommonDivisor = numerator;
-// }
-// console.writeln(`La fracción ${numerator}/${denominator} = ${numerator/greatestCommonDivisor}/\
-// ${denominator/greatestCommonDivisor} invertida es la fracción ${denominator/greatestCommonDivisor}/\
-// ${numerator/greatestCommonDivisor}`);
-
 const { Console } = require("console-mpds");
 const console = new Console();
 
