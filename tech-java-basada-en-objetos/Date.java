cc8b252efadd4ab6d2f59231220fcb8b5d5a7e39
Gabriel Góngora <45207163+Touchsoftwares@users.noreply.github.com>
2023-02-03 12:13:34 -0600

    initial objects in java

--- tech-java-basada-en-objetos/Date.java
+++ tech-java-basada-en-objetos/Date.java
@@ -11,19 +11,27 @@ public class Date {
     public Date(int day,int month,int year) {} 
     public Date(double unixTime) {} 
 
-    public Date clone() {}
-    public String toString() {}
-    public double getUnixTime() {}
-    public int getDay() {}
-    public int getMonth() {}
-    public int getYear() {}
-    public int getWeek() {}
+    public Date clone() {
+        return null;}
+    public String toString() {
+        return template;}
+    public double getUnixTime() {
+        return day;}
+    public int getDay() {
+        return day;}
+    public int getMonth() {
+        return day;}
+    public int getYear() {
+        return day;}
+    public int getWeek() {
+        return day;}
     public bool isLapYear() {}
 
    
     public void setFormatDate(String template) {}
     public void setUnixTime(double unixtime) {}
 
-    public bool equals(Date date) {}
-    private bool checkDate(Date date) {}
+    public boolean equals(Date date) {
+        return false;}
+    
     }
